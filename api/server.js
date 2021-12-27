413552b6dc908606d9c1f20dbfe10dd207396ed8
Alex Fourmy <alexfourmy@gmail.com>
2021-12-27 19:51:31 +0100

    Refactored the logic again to work on the multer issue

--- api/server.js
+++ api/server.js
@@ -5,13 +5,19 @@ const userRoutes = require('./routes/user.routes')
 const postRoutes = require('./routes/post.routes')
 require('dotenv').config({ path: './config/.env' })
 require('./config/db')
-const app = express()
 const { checkUser, requireAuth } = require('./middleware/auth.middleware')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const app = express()
 
 // Middleware
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(helmet())
+app.use(morgan('common'))
 
 // JWT
 app.get('*', checkUser)
