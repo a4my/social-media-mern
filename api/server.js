2ca8e53c5f82ee5d225b6368d34f7b0af7d68e28
Alex Fourmy <alexfourmy@gmail.com>
2021-12-28 18:29:14 +0100

    Trying to fix the profile picture upload functionality with express-upload instead of multer

--- api/server.js
+++ api/server.js
@@ -7,17 +7,62 @@ require('dotenv').config({ path: './config/.env' })
 require('./config/db')
 const { checkUser, requireAuth } = require('./middleware/auth.middleware')
 const cors = require('cors')
-const morgan = require('morgan')
-const helmet = require('helmet')
+const fileUpload = require('express-fileupload')
+const { uploadErrors } = require('./utils/errors.utils')
+const UserModel = require('./models/user.model')
+
 const app = express()
+
+// Avatar upload
+app.use(fileUpload())
+app.post('/api/user/upload', async (req, res) => {
+  // check for errors
+  try {
+    if (!req.files.file.name.match(/\.(png|jpg|jpeg)$/))
+      throw Error('invalid file')
+    if (req.files.file.size > 500000) throw Error('max size')
+    if (!req.files || Object.keys(req.files).length === 0)
+      throw Error('no file')
+  } catch (err) {
+    const errors = uploadErrors(err)
+    return res.status(201).json({ errors })
+  }
+
+  // change the name of the file to the user's name
+  const fileName = req.body.name + '.jpg'
+  req.files.file.name = fileName
+
+  // store the uploaded file into a variable
+  let uploadedFile = req.files.file
+  let uploadPath = `${__dirname}/../client/public/uploads/profil/${fileName}`
+
+  // store the uploaded file on the server
+  await uploadedFile.mv(uploadPath, err => {
+    if (err) return res.status(500).send(err)
+    res.send('File uploaded!')
+  })
+
+  // Add to datatbase
+  try {
+    await UserModel.findByIdAndUpdate(
+      req.body.userId,
+      { $set: { picture: './uploads/profil/' + fileName } },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+      (err, docs) => {
+        if (!err) return res.send(docs)
+        else return res.status(500).send({ message: err })
+      }
+    )
+  } catch (err) {
+    return res.status(500).send({ message: err })
+  }
+})
 
 // Middleware
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(helmet())
-app.use(morgan('common'))
 
 // JWT
 app.get('*', checkUser)
