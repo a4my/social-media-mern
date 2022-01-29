6e6581a8edf98115c0c56f4d58b988469f853dbf
Alex Fourmy <alexfourmy@gmail.com>
2022-01-29 09:19:07 +0100

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles/index.scss'
+import Layout from './components/layout/Layout'
+
+export default function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  )
+}
