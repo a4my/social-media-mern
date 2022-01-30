7639fe77071ddbdf09af732e23e33db49dd17184
Alex Fourmy <alexfourmy@gmail.com>
2022-01-30 14:20:40 +0100

    First commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// export default function Home() {
+//   return <div>Hello from Home</div>
+// }
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello from Home</h1>
+    </div>
+  )
+}
+
+export default Home
