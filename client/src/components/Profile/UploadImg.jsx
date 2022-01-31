fe50389990b477178158ee225a743a82a02ad03f
Alex Fourmy <alexfourmy@gmail.com>
2022-01-31 14:51:53 +0100

    Completed the upload profile picture functionality of the profile page

---
+++ client/src/components/Profile/UploadImg.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { uploadPicture } from '../../actions/user.actions'
+
+const UploadImg = () => {
+  const [file, setFile] = useState()
+  const dispatch = useDispatch()
+  const userData = useSelector(state => state.userReducer)
+
+  const handlePicture = e => {
+    e.preventDefault()
+    const data = new FormData()
+    data.append('name', userData.pseudo)
+    data.append('userId', userData._id)
+    data.append('file', file)
+
+    console.log(data)
+
+    dispatch(uploadPicture(data, userData._id))
+  }
+
+  return (
+    <form action="" onSubmit={handlePicture} className="upload-pic">
+      <label htmlFor="file">Select new picture</label>
+      <input
+        type="file"
+        id="file"
+        name="file"
+        accept=".jpg, .jpeg, .png"
+        onChange={e => setFile(e.target.files[0])}
+      />
+      <br />
+      <input type="submit" value="Send" />
+    </form>
+  )
+}
+
+export default UploadImg
