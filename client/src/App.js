d6e24dc5c212418132ad768b5bc396c9a6c1e90f
Alex Fourmy <alexfourmy@gmail.com>
2022-01-31 14:34:19 +0100

    created a context api and wrapped it around the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles/index.scss'
 import Layout from './components/layout/Layout'
+import { UidContext } from './components/layout/AppContext'
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { getUser } from './actions/user.actions'
 
 export default function App() {
+  const [uid, setUid] = useState(null)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true
+      })
+        .then(res => {
+          setUid(res.data)
+        })
+        .catch(err => console.log('No token'))
+    }
+    fetchToken()
+
+    if (uid) dispatch(getUser(uid))
+  }, [uid, dispatch])
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Layout />
-    </div>
+    </UidContext.Provider>
   )
 }
