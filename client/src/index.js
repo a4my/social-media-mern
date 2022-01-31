20b69e8e1dd9aa579b8844f5020ffb1925eb1433
Alex Fourmy <alexfourmy@gmail.com>
2022-01-31 14:36:32 +0100

    Configured redux and a redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Redux dev tools
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { getUsers } from './actions/users.actions'
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+store.dispatch(getUsers())
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
