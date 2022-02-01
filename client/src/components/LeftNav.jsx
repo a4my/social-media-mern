09fce7168505a9294d42835081b3d90f5aa6f3d1
Alex Fourmy <alexfourmy@gmail.com>
2022-02-01 14:27:26 +0100

    Added the new pages to the left bar

--- client/src/components/LeftNav.jsx
+++ client/src/components/LeftNav.jsx
@@ -6,18 +6,50 @@ export default function LeftNav() {
     <div className="left-nav-container">
       <div className="icons">
         <div className="icons-bis">
-          <NavLink to="/" className="active-left-nav">
-            <img src="./img/icons/home.svg" alt="home icon" />
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              '' + (isActive ? 'active-left-nav' : '')
+            }
+          >
+            <img src="./img/icons/home.svg" alt="home" />
           </NavLink>
           <br />
-          <NavLink to="/trending" className="active-left-nav">
-            <img src="./img/icons/rocket.svg" alt="trending icon" />
+          <NavLink
+            to="/trending"
+            className={({ isActive }) =>
+              '' + (isActive ? 'active-left-nav' : '')
+            }
+          >
+            <img src="./img/icons/trending.svg" alt="trending" />
           </NavLink>
           <br />
-          <NavLink to="/profil" className="active-left-nav">
-            <img src="./img/icons/user.svg" alt="profil icon" />
+          <NavLink
+            to="/profil"
+            className={({ isActive }) =>
+              '' + (isActive ? 'active-left-nav' : '')
+            }
+          >
+            <img src="./img/icons/gear.svg" alt="settings" />
           </NavLink>
           <br />
+          <NavLink
+            to="/profil"
+            className={({ isActive }) =>
+              '' + (isActive ? 'active-left-nav' : '')
+            }
+          >
+            <img src="./img/icons/user.svg" alt="profil" />
+          </NavLink>
+          <br />
+          <NavLink
+            to="/profil"
+            className={({ isActive }) =>
+              '' + (isActive ? 'active-left-nav' : '')
+            }
+          >
+            <img src="./img/icons/chat.svg" alt="chat" />
+          </NavLink>
         </div>
       </div>
     </div>
