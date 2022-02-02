7444e0c94dfe6f6d6ec995d7c2991707915ea59a
Alex Fourmy <alexfourmy@gmail.com>
2022-02-02 15:19:00 +0100

    Added a new route for the like & unlike post button

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -3,13 +3,12 @@ const postController = require('../controllers/post.controller')
 const multer = require('multer')
 const upload = multer()
 
-// posts
 router.get('/', postController.readPost)
+router.get('/:id', postController.getPost)
 router.post('/', upload.single('file'), postController.createPost)
 router.put('/:id', postController.updatePost)
 router.delete('/:id', postController.deletePost)
-router.patch('/like-post/:id', postController.likePost)
-router.patch('/unlike-post/:id', postController.unlikePost)
+router.put('/:id/like', postController.likeUnlikePost)
 
 // comments
 router.patch('/comment-post/:id', postController.commentPost)
