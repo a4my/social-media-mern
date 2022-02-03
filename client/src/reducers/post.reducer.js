76e85bbf3be3d4ff99f624dfb5c9c6f440106cf2
Alex Fourmy <alexfourmy@gmail.com>
2022-02-03 14:51:56 +0100

    Added the comment actions to the reducer

--- client/src/reducers/post.reducer.js
+++ client/src/reducers/post.reducer.js
@@ -1,4 +1,11 @@
-import { GET_POSTS, LIKE_UNLIKE_POST } from '../actions/post.actions'
+import {
+  DELETE_COMMENT,
+  DELETE_POST,
+  EDIT_COMMENT,
+  GET_POSTS,
+  LIKE_UNLIKE_POST,
+  UPDATE_POST
+} from '../actions/post.actions'
 
 const initialState = {}
 
@@ -22,6 +29,46 @@ export default function postReducer(state = initialState, action) {
           }
         }
         return post
+      })
+    case UPDATE_POST:
+      return state.map(post => {
+        if (post._id === action.payload.postId) {
+          return {
+            ...post,
+            message: action.payload.message
+          }
+        } else return post
+      })
+    case EDIT_COMMENT:
+      return state.map(post => {
+        if (post._id === action.payload.postId) {
+          return {
+            ...post,
+            comments: post.comments.map(comment => {
+              if (comment._id === action.payload.commentId) {
+                return {
+                  ...comment,
+                  text: action.payload.text
+                }
+              } else {
+                return comment
+              }
+            })
+          }
+        } else return post
+      })
+    case DELETE_POST:
+      return state.filter(post => post._id !== action.payload.postId)
+    case DELETE_COMMENT:
+      return state.map(post => {
+        if (post._id === action.payload.postId) {
+          return {
+            ...post,
+            comments: post.comments.filter(
+              comment => comment._id !== action.payload.commentId
+            )
+          }
+        } else return post
       })
     default:
       return state
