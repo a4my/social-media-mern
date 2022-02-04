164e2d5c468cead84f992fb341c42764fdb0d0a8
Alex Fourmy <alexfourmy@gmail.com>
2022-02-04 19:15:08 +0100

    Work onthe trending page and added the trend actions to the reducer

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 // posts
 export const GET_POSTS = 'GET_POSTS'
+export const GET_ALL_POSTS = 'GET_ALL_POSTS'
+export const ADD_POST = 'ADD_POST'
 export const LIKE_UNLIKE_POST = 'LIKE_UNLIKE_POST'
 export const UPDATE_POST = 'UPDATE_POST'
 export const DELETE_POST = 'DELETE_POST'
@@ -11,6 +13,12 @@ export const ADD_COMMENT = 'ADD_COMMENT'
 export const EDIT_COMMENT = 'EDIT_COMMENT'
 export const DELETE_COMMENT = 'DELETE_COMMENT'
 
+// Errors
+export const GET_POST_ERRORS = 'GET_POST_ERRORS'
+
+// trends
+export const GET_TRENDS = 'GET_TRENDS'
+
 // post actions
 export const getPosts = num => {
   return dispatch => {
@@ -19,8 +27,23 @@ export const getPosts = num => {
       .then(res => {
         const array = res.data.slice(0, num)
         dispatch({ type: GET_POSTS, payload: array })
+        dispatch({ type: GET_ALL_POSTS, payload: res.data })
       })
       .catch(err => console.log(err))
+  }
+}
+
+export const addPost = data => {
+  return dispatch => {
+    return axios
+      .post(`${process.env.REACT_APP_API_URL}api/post/`, data)
+      .then(res => {
+        if (res.data.errors) {
+          dispatch({ type: GET_POST_ERRORS, payload: res.data.errors })
+        } else {
+          dispatch({ type: GET_POST_ERRORS, payload: '' })
+        }
+      })
   }
 }
 
@@ -105,5 +128,12 @@ export const deleteComment = (postId, commentId) => {
         dispatch({ type: DELETE_COMMENT, payload: { postId, commentId } })
       })
       .catch(err => console.log(err))
+  }
+}
+
+// trends actions
+export const getTrends = sortedArray => {
+  return dispatch => {
+    dispatch({ type: GET_TRENDS, payload: sortedArray })
   }
 }
