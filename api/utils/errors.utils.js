80c7826c533975dec4ae75fe0f3a09604ed3a87d
Alex Fourmy <alexfourmy@gmail.com>
2022-02-04 19:11:23 +0100

    Looking for an issue with the errors rendering on the frontend

--- api/utils/errors.utils.js
+++ api/utils/errors.utils.js
@@ -2,18 +2,18 @@ module.exports.signUpErrors = err => {
   let errors = { pseudo: '', email: '', password: '' }
 
   if (err.message.includes('pseudo'))
-    errors.pseudo = 'Username is incorrect or already taken'
+    errors.pseudo = 'Username is invalid or already taken'
 
-  if (err.message.includes('email')) errors.email = 'Email address is incorrect'
+  if (err.message.includes('email')) errors.email = 'Email is incorrect'
 
   if (err.message.includes('password'))
-    errors.password = 'Password must be at least 6 characters'
+    errors.password = 'Password must be longer than 6 characters'
 
   if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('pseudo'))
-    errors.pseudo = 'This username has already been taken'
+    errors.pseudo = 'Username is already taken'
 
   if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email'))
-    errors.email = 'This email address has already been used'
+    errors.email = 'Email address has already been registered'
 
   return errors
 }
@@ -21,23 +21,20 @@ module.exports.signUpErrors = err => {
 module.exports.signInErrors = err => {
   let errors = { email: '', password: '' }
 
-  if (err.message.includes('email')) errors.email = 'Email address is incorrect'
+  if (err.message.includes('email')) errors.email = 'Invalid email'
 
   if (err.message.includes('password'))
-    errors.password = 'Password is incorrect'
+    errors.password = "Password doesn't match"
 
   return errors
 }
 
 module.exports.uploadErrors = err => {
-  let errors = { format: '', maxSize: '', noFile: '' }
+  let errors = { format: '', maxSize: '' }
 
-  if (err.message.includes('invalid file')) errors.format = 'Wrong format!'
+  if (err.message.includes('invalid file')) errors.format = 'Invalid format'
 
   if (err.message.includes('max size')) errors.maxSize = 'File exceeds 500kb'
-
-  if (err.message.includes('no file'))
-    errors.noFile = 'No files were selected or uploaded'
 
   return errors
 }
