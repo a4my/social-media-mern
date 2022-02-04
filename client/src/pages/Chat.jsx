90ce411be21a083fd06dee44f7a4127283eda9f4
Alex Fourmy <alexfourmy@gmail.com>
2022-02-04 20:01:52 +0100

    Initiated the chat page

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LeftNav from '../components/LeftNav'
+
+export default function Chat() {
+  return (
+    <>
+      <LeftNav />
+      <div>Chat section under construction</div>
+    </>
+  )
+}
