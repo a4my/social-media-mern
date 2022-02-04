7cbe9d7243ad47716418bca7c8e57590cd8cc801
Alex Fourmy <alexfourmy@gmail.com>
2022-02-04 20:03:18 +0100

    Started working on the user profile page

--- client/src/pages/Profil.jsx
+++ client/src/pages/Profil.jsx
@@ -1,23 +1,34 @@
 import React, { useContext } from 'react'
-import Auth from '../components/auth/Auth'
 import { UidContext } from '../components/layout/AppContext'
-import UpdateProfile from '../components/Profile/UpdateProfile'
+import LeftNav from '../components/LeftNav'
+import NewPostForm from '../components/Post/NewPostForm'
+import Thread from '../components/Thread'
+import Auth from '../components/auth/Auth'
+import FriendsHint from '../components/Profile/FriendsHint'
+import FriendsList from '../components/Profile/FriendsList'
 
-export default function Profil() {
+const Home = () => {
   const uid = useContext(UidContext)
 
   return (
-    <div className="profil-page">
-      {uid ? (
-        <UpdateProfile />
-      ) : (
-        <div className="log-container">
-          <Auth signin={false} signup={true} />
-          <div className="img-container">
-            <img src="./img/log.svg" alt="img log" />
+    <div className="profil">
+      <LeftNav />
+      <div className="main">
+        <div className="profil-header">
+          {uid ? <NewPostForm /> : <Auth signin={true} signup={false} />}
+        </div>
+        {/* {uid ? <Thread /> : ''} */}
+      </div>
+      <div className="right-side">
+        <div className="right-side-container">
+          <div className="wrapper">
+            {uid && <FriendsList />}
+            {uid && <FriendsHint />}
           </div>
         </div>
-      )}
+      </div>
     </div>
   )
 }
+
+export default Home
