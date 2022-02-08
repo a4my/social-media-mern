359ee756c27aa62d4f600e3c7145f393764f0981
Alex Fourmy <alexfourmy@gmail.com>
2022-02-08 18:58:31 +0100

    Refactored the code to try and fix an issue with the Thread component on siging in

---
+++ client/src/reducers/followingPosts.js
@@ -0,0 +1,16 @@
+// import { GET_FOLLOWING_POSTS } from '../actions/post.actions'
+
+// const initialState = {}
+
+// export default function followingReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case GET_FOLLOWING_POSTS:
+//       return {
+//         ...state,
+//         action
+//       }
+
+//     default:
+//       return state
+//   }
+// }
