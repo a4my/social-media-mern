4e8f59766319c07838f4dd53085c499f8efb0dbd
Alex Fourmy <alexfourmy@gmail.com>
2022-02-09 19:46:22 +0100

    Created new routes and actions to update user information

--- client/src/components/Profile/UpdateProfile.jsx
+++ client/src/components/Profile/UpdateProfile.jsx
@@ -3,13 +3,24 @@ import LeftNav from '../LeftNav'
 import { useSelector, useDispatch } from 'react-redux'
 import UploadImg from './UploadImg'
 import { useState } from 'react'
-import { updateBio } from '../../actions/user.actions'
+import {
+  updateBio,
+  updateCityDb,
+  updateFromDb,
+  updateJobDb
+} from '../../actions/user.actions'
 import { dateParser } from '../utils'
 import FollowHandler from './FollowHandler'
 
 export default function UpdateProfile() {
   const [bio, setBio] = useState('')
+  const [city, setCity] = useState('')
+  const [from, setFrom] = useState('')
+  const [job, setJob] = useState('')
   const [updateForm, setUpdateForm] = useState(false)
+  const [updateCity, setUpdateCity] = useState(false)
+  const [updateFrom, setUpdateFrom] = useState(false)
+  const [updateJob, setUpdateJob] = useState(false)
   const userData = useSelector(state => state.userReducer)
   const usersData = useSelector(state => state.usersReducer)
   const dispatch = useDispatch()
@@ -19,6 +30,21 @@ export default function UpdateProfile() {
   const handleUpdate = () => {
     dispatch(updateBio(userData._id, bio))
     setUpdateForm(false)
+  }
+
+  const handleCity = () => {
+    dispatch(updateCityDb(userData._id, city))
+    setUpdateCity(false)
+  }
+
+  const handleFrom = () => {
+    dispatch(updateFromDb(userData._id, from))
+    setUpdateFrom(false)
+  }
+
+  const handleJob = () => {
+    dispatch(updateJobDb(userData._id, job))
+    setUpdateJob(false)
   }
 
   return (
@@ -36,15 +62,14 @@ export default function UpdateProfile() {
           {/* <p>{errors.maxSize}</p>
           <p>{errors.format}</p> */}
         </div>
+
         <div className="right-part">
           <div className="bio-update">
             <h3>About me</h3>
             {updateForm !== true && (
               <>
                 <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
-                <button onClick={() => setUpdateForm(!updateForm)}>
-                  Change
-                </button>
+                <button onClick={() => setUpdateForm(!updateForm)}>Edit</button>
               </>
             )}
             {updateForm && (
@@ -80,6 +105,60 @@ export default function UpdateProfile() {
               ? 's'
               : null}: {userData.followers ? userData.followers.length : ''}
           </h5>
+        </div>
+        <div className="info">
+          <h3>Personal Information</h3>
+          {updateCity === false && (
+            <div className="info-container">
+              <p>City: {userData.city}</p>
+              <button onClick={() => setUpdateCity(!updateCity)}>Edit</button>
+            </div>
+          )}
+          {updateCity && (
+            <div className="info-container">
+              <textarea
+                type="text"
+                maxLength="20"
+                defaultValue={userData.city}
+                onChange={e => setCity(e.target.value)}
+              ></textarea>
+              <button onClick={handleCity}>Save</button>
+            </div>
+          )}
+          {updateFrom === false && (
+            <div className="info-container">
+              <p>From: {userData.from}</p>
+              <button onClick={() => setUpdateFrom(!updateFrom)}>Edit</button>
+            </div>
+          )}
+          {updateFrom && (
+            <div className="info-container">
+              <textarea
+                type="text"
+                maxLength="20"
+                defaultValue={userData.from}
+                onChange={e => setFrom(e.target.value)}
+              ></textarea>
+              <button onClick={handleFrom}>Save</button>
+            </div>
+          )}
+          {updateJob === false && (
+            <div className="info-container">
+              <p>Occupation: {userData.job}</p>
+              <button onClick={() => setUpdateJob(!updateJob)}>Edit</button>
+            </div>
+          )}
+          {updateJob && (
+            <div className="info-container">
+              <textarea
+                type="text"
+                maxLength="20"
+                defaultValue={userData.job}
+                onChange={e => setJob(e.target.value)}
+              ></textarea>
+              <button onClick={handleJob}>Save</button>
+            </div>
+          )}
         </div>
       </div>
 
