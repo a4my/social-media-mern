4c37f18d7e29aeaf02edd3cc0a6c65ea0ee81417
Alex Fourmy <alexfourmy@gmail.com>
2022-02-10 18:51:53 +0100

    Tried refactoring the code to track a new issue with the like unlike post functionality

--- client/src/components/Post/LikeButton.jsx
+++ client/src/components/Post/LikeButton.jsx
@@ -1,18 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { UidContext } from '../layout/AppContext'
-import Popup from 'reactjs-popup'
-import 'reactjs-popup/dist/index.css'
 import { useDispatch } from 'react-redux'
-import { likeUnlikePost } from '../../actions/post.actions'
+import { likePost, unlikePost } from '../../actions/post.actions'
 
 const LikeButton = ({ post }) => {
   const [liked, setLiked] = useState(false)
   const uid = useContext(UidContext)
   const dispatch = useDispatch()
 
-  const likeHandler = () => {
-    dispatch(likeUnlikePost(post._id, uid))
-    setLiked(!liked)
+  const like = () => {
+    dispatch(likePost(post._id, uid))
+    setLiked(true)
+  }
+
+  const unlike = () => {
+    dispatch(unlikePost(post._id, uid))
+    setLiked(false)
   }
 
   useEffect(() => {
@@ -22,24 +25,11 @@ const LikeButton = ({ post }) => {
 
   return (
     <div className="like-container">
-      {uid === null && (
-        <Popup
-          trigger={<img src="./img/icons/heart.svg" alt="like" />}
-          position={['bottom center', 'bottom right', 'bottom left']}
-          closeOnDocumentClick
-        >
-          <div>Please login to like a post</div>
-        </Popup>
-      )}
       {uid && liked === false && (
-        <img src="./img/icons/heart.svg" onClick={likeHandler} alt="like" />
+        <img src="./img/icons/heart.svg" onClick={like} alt="like" />
       )}
       {uid && liked && (
-        <img
-          src="./img/icons/heart-filled.svg"
-          onClick={likeHandler}
-          alt="unlike"
-        />
+        <img src="./img/icons/heart-filled.svg" onClick={unlike} alt="unlike" />
       )}
       <span>{post.likers.length}</span>
     </div>
