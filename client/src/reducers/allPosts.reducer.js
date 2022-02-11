6dde0bd2a889b1d4a1ffb00ce804893e1de598f1
Alex Fourmy <alexfourmy@gmail.com>
2022-02-11 11:44:27 +0100

    Fix an issue with the like and unlike post functionality

--- client/src/reducers/allPosts.reducer.js
+++ client/src/reducers/allPosts.reducer.js
@@ -1,4 +1,4 @@
-import { GET_ALL_POSTS } from '../actions/post.actions'
+import { GET_ALL_POSTS, LIKE_POST, UNLIKE_POST } from '../actions/post.actions'
 
 const initialState = {}
 
@@ -6,6 +6,26 @@ export default function allPostsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_ALL_POSTS:
       return action.payload
+    case LIKE_POST:
+      return state.map(post => {
+        if (post._id === action.payload.postId) {
+          return {
+            ...post,
+            likers: [action.payload.userId, ...post.likers]
+          }
+        }
+        return post
+      })
+    case UNLIKE_POST:
+      return state.map(post => {
+        if (post._id === action.payload.postId) {
+          return {
+            ...post,
+            likers: post.likers.filter(id => id !== action.payload.userId)
+          }
+        }
+        return post
+      })
     default:
       return state
   }
