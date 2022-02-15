3f884c9ad8d2baa7aa922ed8e19f8048937e0b27
Alex Fourmy <alexfourmy@gmail.com>
2022-02-15 18:53:33 +0100

    Refactored the code to fix an issue with users profile rendering

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -11,8 +11,9 @@ module.exports.userInfo = (req, res) => {
     return res.status(400).send('ID unknown: ' + req.params.id)
 
   UserModel.findById(req.params.id, (err, docs) => {
-    if (!err) res.send(docs)
-    else console.log('ID unknown: ' + err)
+    if (!err) {
+      res.send(docs)
+    } else console.log('ID unknown: ' + err)
   }).select('-password')
 }
 
